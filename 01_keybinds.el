4e8a034fbc9bac39ce3301bc597c90c6e2ee6309
miya789 <miya.nori42395@gmail.com>
2018-03-27 14:16:33 +0900

    Added testfile

--- 01_keybinds.el
+++ 01_keybinds.el
@@ -1,4 +1,25 @@
-(global-set-key "\C-z" 'undo)
+;; ----------------------------------------
+;; package.el
+;; ----------------------------------------
+(require 'package)
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; MELPA-stableを追加
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+;; 初期化
+(package-initialize)
 
+;; auto-highlight-symbol
 (require 'auto-highlight-symbol-config)
-;; M-x el-get-install auto-highlight-symbol
+
+;; highlight-symbol
+(require 'highlight-symbol)
+(setq highlight-symbol-colors '("DarkOrange" "DodgerBlue1" "DeepPink1")) ;; 使いたい色を設定、repeatしてくれる
+
+;; 適宜keybindの設定
+(global-set-key (kbd "<f3>") 'highlight-symbol-at-point)
+(global-set-key (kbd "M-<f3>") 'highlight-symbol-remove-all)
+
+(global-set-key "\C-u" 'scroll-down-command)
+(global-set-key "\C-d" 'scroll-up-command)
+(global-set-key "\C-z" 'undo)
